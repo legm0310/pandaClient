6755dc5d0efb08ddd275f94697e8413741031989
Lee gyu min <legm0310@gmail.com>
2023-05-16 11:09:29 +0900

    env

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: process.env.SERVER_URL,
+      target: process.env.API_BASE_URL,
       chagneOrigin: true,
     })
   );
