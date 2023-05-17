143ffefc5c4ee567738e742504d33f32bbce06cf
legm0310 <legm0310@email.com>
2023-05-18 05:35:20 +0900

    Fix: proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    "/api",
+    createProxyMiddleware({
       target: process.env.REACT_APP_API_BASE_URL,
       changeOrigin: true,
     })
