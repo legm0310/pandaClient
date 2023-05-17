58ee326802537e7693951996f9a1d9845b030cbf
legm0310 <legm0310@email.com>
2023-05-18 05:25:30 +0900

    Fix: proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
+    createProxyMiddleware("https://panda-liart.vercel.app/api", {
       target: process.env.REACT_APP_API_BASE_URL,
       changeOrigin: true,
     })
