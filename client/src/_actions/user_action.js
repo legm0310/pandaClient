5db3a1c193c77d5f83d17ab111c38553c894cb3e
legm0310 <legm0310@email.com>
2023-05-21 00:49:31 +0900

    Fix: action 함수

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -9,7 +9,7 @@ import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "./type";
 
 export function loginUser(dataToSubmit) {
   const request = axios
-    .post("/api/auth/login", dataToSubmit)
+    .post(`${process.env.REACT_APP_API_BASE_URL}/api/auth/login`, dataToSubmit)
     .then((response) => {
       console.log(response.headers);
       let accessToken = response.headers.get("Authorization");
