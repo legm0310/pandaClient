62412fbdb4fccd7da35a403e5336b7088d95a57c
Lee Gyumin <legm0310@gmail.com>
2023-10-07 23:18:33 +0900

    Refactor: api 함수 일반화

--- client/src/api/common.js
+++ client/src/api/common.js
@@ -43,7 +43,6 @@ export const interceptRes = (instance) => {
 export const baseRequest = (options) => {
   const instance = axios.create({
     baseURL: apiBaseUrl,
-    withCredentials: true,
     ...options,
   });
   const intercept = interceptRes(instance);
